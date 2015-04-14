4038196d2bb503c8d58c81e83949a176a8cda004
Jessica Kerr <jessitron@outpace.com>
2015-04-14 20:05:09 +0000

    Remove unused method, i think

--- slack-gen/src/slack_gen/mygen.clj
+++ slack-gen/src/slack_gen/mygen.clj
@@ -13,10 +13,6 @@
 (def channel-name-char (gen/elements t/channel-name-allowed-chars))
 (def disallowed? (partial contains? #{"_" "-"}))
 
-;; Can't be all underscores, or all dashes. Can be a combo of the two
-;; (by observation)
-(defn disallowed? [channel-name]
-  (or (t/all-underscores? channel-name) (t/all-dashes? channel-name)))
 
 (def channel-name (gen/no-shrink
                   (gen/such-that (complement disallowed?)
