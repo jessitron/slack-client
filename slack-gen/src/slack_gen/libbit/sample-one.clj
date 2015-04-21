3bbc787e22054e22980fff361aa74c2fe7fd798d
Jessica Kerr <jessitron@outpace.com>
2015-04-21 00:34:13 +0000

    what is this doing here argh

--- slack-gen/src/slack_gen/libbit/sample-one.clj
+++
@@ -1,7 +0,0 @@
-(ns slack-gen.libbit.sample-one
-  (:require [clojure.test.check.generators :as gen]
-            [schema.core :as s]))
-
-;; TODO - this is where I want to say "generator of A" and "this returns an A"
-(s/defn sample-one [g :- (s/record clojure.test.check.generators.Gen)]
-  (last (gen/sample g)))
